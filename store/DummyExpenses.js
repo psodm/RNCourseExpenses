0dfff7e1f7da52f57e87f92142915b8b3d775dc9
psodm <peter.stacey@broadcom.com>
2024-11-30 18:27:15 +1100

    formatted expense context

--- store/DummyExpenses.js
+++ store/DummyExpenses.js
@@ -1,12 +1,62 @@
 export const DUMMY_EXPENSES = [
-    {id: 'e1', description: 'A pair of shows', amount: 59.99, date: new Date('2024-11-29')},
-    {id: 'e2', description: 'A pair of trousers', amount: 89.29, date: new Date('2024-11-27')},
-    {id: 'e3', description: 'Bananas', amount: 5.99, date: new Date('2024-11-24')},
-    {id: 'e4', description: 'Book', amount: 14.99, date: new Date('2024-11-29')},
-    {id: 'e5', description: 'Lunch', amount: 18.59, date: new Date('2024-11-28')},
-    {id: 'e6', description: 'A pair of shows', amount: 59.99, date: new Date('2024-11-29')},
-    {id: 'e7', description: 'A pair of trousers', amount: 89.29, date: new Date('2024-11-27')},
-    {id: 'e8', description: 'Bananas', amount: 5.99, date: new Date('2024-11-24')},
-    {id: 'e9', description: 'Book', amount: 14.99, date: new Date('2024-11-29')},
-    {id: 'e10', description: 'Lunch', amount: 18.59, date: new Date('2024-11-28')}
-]
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2024-11-29"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2024-11-1"),
+  },
+  {
+    id: "e3",
+    description: "Bananas",
+    amount: 5.99,
+    date: new Date("2024-11-24"),
+  },
+  {
+    id: "e4",
+    description: "Book",
+    amount: 14.99,
+    date: new Date("2024-11-29"),
+  },
+  {
+    id: "e5",
+    description: "Lunch",
+    amount: 18.59,
+    date: new Date("2024-11-28"),
+  },
+  {
+    id: "e6",
+    description: "A pair of shows",
+    amount: 59.99,
+    date: new Date("2024-11-29"),
+  },
+  {
+    id: "e7",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2024-11-27"),
+  },
+  {
+    id: "e8",
+    description: "Bananas",
+    amount: 5.99,
+    date: new Date("2024-11-24"),
+  },
+  {
+    id: "e9",
+    description: "Book",
+    amount: 14.99,
+    date: new Date("2024-11-29"),
+  },
+  {
+    id: "e10",
+    description: "Lunch",
+    amount: 18.59,
+    date: new Date("2024-11-28"),
+  },
+];
